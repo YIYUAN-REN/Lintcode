fe91a4d7bc82538804b2fec4395e6a32bf9e76ba
LoweRen <Loweryy1017@gmail.com>
2021-12-15 00:32:01 -0800

    add binary search required

---
+++ Binary Search/Required/1_x的n次方.py
@@ -0,0 +1,38 @@
+﻿# Pow(x, n)
+
+"""
+递归
+1. base case: pow(x, 0) = 1
+2. recursive rule: 
+    if n是偶数, pow(x, n) = pow(x, n//2) * pow(x, n//2)
+    if n是奇数, pow(x, n) = pow(x, n//2) * pow(x, n//2) * x
+注意：检查
+    分母为0情况
+    n为负的情况
+"""
+
+class Solution:
+    """
+    @param x {float}: the base number
+    @param n {int}: the power number
+    @return {float}: the result
+    """
+    def myPow(self, x, n):
+        # write your code here
+        if x == 0 and n <= 0:
+            return -1
+        elif n < 0:
+            return self.pow(1 / x, -n)
+        else:
+            return self.pow(x, n)
+    
+    
+    def pow(self, x, n):
+        if n == 0:
+            return 1
+        
+        half_result = self.pow(x, n // 2)
+        if n % 2 == 1:
+            return half_result * half_result * x
+        else:
+            return half_result * half_result
---
+++ Binary Search/Required/2_快速次方.py
@@ -0,0 +1,29 @@
+﻿# Fast Power
+
+"""
+题目目的：a^n大到超出表达范围怎么计算a^n%b
+
+递归
+1. base case：若n=0，返回1%b
+2. recursive：
+    若n为偶数，返回(half_result*half_result)%b
+    若n为奇数，返回(half_result*half_result*a)%b
+"""
+
+class Solution:
+    """
+    @param a: A 32bit integer
+    @param b: A 32bit integer
+    @param n: A 32bit integer
+    @return: An integer
+    """
+    def fastPower(self, a, b, n):
+        # write your code here
+        if n == 0:
+            return 1 % b
+        
+        half_result = self.fastPower(a, b, n // 2)
+        if n % 2 == 0:
+            return (half_result * half_result) % b
+        else:
+            return (half_result * half_result * a) % b
